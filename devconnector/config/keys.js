9ffec9c2d721403c932ca0c5be9a4aa3b7c4fee9
Brayn <ilogirb23@yahoo.com>
2018-06-26 16:08:07 +0800

    added gitignore 9

--- devconnector/config/keys.js
+++ devconnector/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
 	mongoURI: 'mongodb://brigz123:brigz123@ds013564.mlab.com:13564/devconnector',//user and password is brigz123
-	secretOrKey: 'secret'
+	secretOrKey: 'secret',
+	asdf:'asdf'
 }
